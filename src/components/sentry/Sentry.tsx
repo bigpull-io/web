c2f2a5b67aa66333ec405c4026fe098249bfb9a9
David Dios <dios.david@thedevcore.net>
2023-07-02 19:09:31 +0100

    add Sentry dsn

--- src/components/sentry/Sentry.tsx
+++ src/components/sentry/Sentry.tsx
@@ -10,6 +10,7 @@ export const Sentry = component$(() => {
     }
 
     sdk.init({
+      dsn: 'https://09cf6de6f97a417c82382297997e9deb@o4505460801798144.ingest.sentry.io/4505460804943872',
       environment: import.meta.env.PROD ? 'production' : 'development',
       release: import.meta.env.APP_VERSION,
       allowUrls: ['https://www.bigpull.io/'],
